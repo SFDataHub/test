b3566ce11151c62f7e9694ad9dca364dae7e3466
Darth Monk <danny.faehnrich@gmail.com>
2025-09-16 06:00:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,4 @@
-<script>
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('✅ Service Worker registered:', reg))
-      .catch(err => console.warn('❌ Service Worker failed:', err));
-  }
-</script>
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+});
+self.addEventListener("fetch", () => {});
