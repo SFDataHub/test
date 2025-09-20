76a5cc2a6f7d63b033335f8a040a33b7aa33c797
Dein Name <deine@mailadresse.tld>
2025-09-20 20:34:18 +0200

    init preview

--- frontend/src/pages/OldScans.tsx
+++
@@ -1,5 +0,0 @@
-import { useTranslation } from 'react-i18next'
-export default function OldScans(){
-  const { t } = useTranslation()
-  return <h1 className="text-2xl font-semibold text-sd-title">{t('page.old.title')}</h1>
-}
--- src/pages/Community.tsx
+++
@@ -1,5 +0,0 @@
-import { useTranslation } from 'react-i18next'
-export default function Community(){
-  const { t } = useTranslation()
-  return <h1 className="text-2xl font-semibold text-sd-title">{t('page.community.title')}</h1>
-}
